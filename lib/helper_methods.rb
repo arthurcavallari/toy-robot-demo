d69b9d4da34778fe8a8254d19bf178cf8dfca00b
Arthur Cavallari <arthur.cavallari@playup.com>
2014-05-27 18:58:34 +1000

    Refactored helper methods to one liners

--- lib/helper_methods.rb
+++ lib/helper_methods.rb
@@ -4,14 +4,10 @@
 
 module HelperMethods
     def numeric? str
-        true if Integer(str)
-    rescue ArgumentError, TypeError
-        false
+        true if Integer(str) rescue false
     end
 
     def to_numeric! str
-        Integer(str)
-    rescue ArgumentError, TypeError
-        nil
+        Integer(str) rescue nil
     end
 end
